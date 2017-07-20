8fa917e117c0bb91eea380044a64e0af414175ab
Sharat Sharma <sharat.sharma@nectechnologies.in>
2017-07-20 15:54:00 +0530

    Switch from oslosphinx to openstackdocstheme
    As part of the docs migration work[0] for Pike we need to switch to use
    the openstackdocstheme.
    
    [0]https://review.openstack.org/#/c/472275/
    
    Change-Id: If242bcfa53a688da52ac197f783f7b6dd2c5e246
    Depends-on: I5d91dcd4a50b148eae4c2bf905cf52c8d5bbbe8e

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -22,8 +21,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
-    #'sphinx.ext.intersphinx',
-    'oslosphinx'
+    'openstackdocstheme'
 ]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
@@ -38,7 +36,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'mistral-lib'
-copyright = u'2016, OpenStack Foundation'
+copyright = u'2017, Mistral Contributors'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
@@ -53,9 +51,8 @@ pygments_style = 'sphinx'
 # -- Options for HTML output --------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
-# Sphinx are currently 'default' and 'sphinxdoc'.
 # html_theme_path = ["."]
-# html_theme = '_theme'
+html_theme = 'openstackdocs'
 # html_static_path = ['static']
 
 # Output file base name for HTML help builder.
@@ -73,3 +70,14 @@ latex_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 #intersphinx_mapping = {'http://docs.python.org/': None}
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+# html_last_updated_fmt = '%b %d, %Y'
+# Must set this variable to include year, month, day, hours, and minutes.
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
+
+# -- Options for openstackdocstheme -------------------------------------------
+repository_name = 'openstack/mistral-lib'
+bug_project = 'mistral'
+bug_tag = ''
