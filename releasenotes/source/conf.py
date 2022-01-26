2f43f651e8a4be8cef838b463e5ec6ab821f2b43
shanyunfan33 <shanyunfan@inspur.com>
2022-01-26 17:29:08 +0800

    remove unicode from code
    remove unicode from code
    
    Change-Id: I4a5ecfccac4047803b069034575bda8e3baae985

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -54,8 +54,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'mistral-lib Release Notes'
-copyright = u'2016, OpenStack Foundation'
+project = 'mistral-lib Release Notes'
+copyright = '2016, OpenStack Foundation'
 
 # Release notes are version independent
 
@@ -200,8 +200,8 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     ('index', 'MistralLibReleaseNotes.tex',
-     u'Mistral Library Release Notes Documentation', u'Mistral '
-     u'Library Developers', 'manual'),
+     'Mistral Library Release Notes Documentation', 'Mistral '
+     'Library Developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -230,8 +230,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'mistrallibreleasenotes', u'Mistral Library Release Notes '
-     u'Documentation', [u'Mistral Library Developers'], 1)
+    ('index', 'mistrallibreleasenotes', 'Mistral Library Release Notes '
+     'Documentation', ['Mistral Library Developers'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -244,8 +244,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'MistralLibReleaseNotes', u'Mistral Library Release Notes '
-     u'Documentation', u'Mistral Library Developers',
+    ('index', 'MistralLibReleaseNotes', 'Mistral Library Release Notes '
+     'Documentation', 'Mistral Library Developers',
      'MistralLibReleaseNotes', 'One line description of project.',
      'Miscellaneous'),
 ]
